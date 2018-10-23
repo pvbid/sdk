990e3aa0bea03da9342501682d97abf3d10d60d0
Randen Kelly <randen@pvbid.com>
2018-10-23 17:28:13 -0600

    Prediction model pricing download

--- src/repositories/PredictionModelRepository.js
+++ src/repositories/PredictionModelRepository.js
@@ -1,18 +1,33 @@
 import BaseRepository from "./BaseRepository";
 
 export default class PredictionModelRepository extends BaseRepository {
-  constructor(config) {
-      super(config.base_uri + "/predictions/", "prediction", "prediction_models", config);
-  }
+    constructor(config) {
+        super(config.base_uri + "/predictions/", "prediction", "prediction_models", config);
+    }
 
-  /**
-   * Retrieves a list of predition models
-   * 
-   * @param {object} params
-   * @param {number} params.lineItemDefId Filters results by line item definition
-   * @return {Promise<BidEntity[]>} List of predictions
-   */
-  async get(params) {
-      return super.get(params);
-  }
+    /**
+     * Retrieves a list of predition models
+     *
+     * @param {object} params
+     * @param {number} params.lineItemDefId Filters results by line item definition
+     * @return {Promise<BidEntity[]>} List of predictions
+     */
+    async get(params) {
+        return super.get(params);
+    }
+
+    /**
+     * Get a prediction model with associated datasets
+     *
+     * @param {number} predictionModelId
+     * @return {promise<BidEntity>} prediction model with datasets
+     */
+    async getData(predictionModelId) {
+        try {
+            const res = await this.http.get(`${this.endpoint}${predictionModelId}/data`);
+            return res.data.data;
+        } catch (error) {
+            return Promise.reject(error);
+        }
+    }
 }
