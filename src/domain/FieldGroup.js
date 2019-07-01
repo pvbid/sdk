bb9ace182579e5052c5fa33f57bd1b3cf0a6329a
Ryan Elliott <ryan@pvbid.com>
2019-07-01 14:45:13 +0000

    Merged in feature/PV-1259-fields-in-field-group (pull request #60)
    feature/PV-1259-fields-in-field-group
    
    * get field entities from group
    
    * merge from dev
    
    * update changelog
    
    Approved-by: Randen Kelly

--- src/domain/FieldGroup.js
+++ src/domain/FieldGroup.js
@@ -6,76 +6,89 @@ import { setAssembly, getAssembly } from "./services/BidEntityAssemblyService";
  * Field Group Class
  */
 export default class FieldGroup extends BidEntity {
+  /**
+   * Creates an instance of FieldGroup.
+   * @param {object} fieldGroupData
+   * @param {Bid} bid
+   */
+  constructor(fieldGroupData, bid) {
+    super();
     /**
-     * Creates an instance of FieldGroup.
-     * @param {object} fieldGroupData 
-     * @param {Bid} bid 
+     * Reference to the bid that the field group belongs to.
+     * @type {Bid}
      */
-    constructor(fieldGroupData, bid) {
-        super();
-        /**
-         * Reference to the bid that the field group belongs to.
-         * @type {Bid}
-         */
-        this.bid = bid;
-        this._original = cloneDeep(fieldGroupData);
-        this._data = fieldGroupData;
-    }
+    this.bid = bid;
+    this._original = cloneDeep(fieldGroupData);
+    this._data = fieldGroupData;
+  }
 
-    /**
-     * Holds the Field Group configuration information.
-     * @type {object}
-     */
-    get config() {
-        return this._data.config;
-    }
+  /**
+   * Holds the Field Group configuration information.
+   * @type {object}
+   */
+  get config() {
+    return this._data.config;
+  }
 
-    /**
-     * Get the field group's assembly if it has one
-     *
-     * @return {Assembly|undefined}
-     */
-    getAssembly() {
-        return getAssembly(this);
-    }
+  /**
+   * Retrieve the fields belonging to the group keyed by reference
+   *
+   * @return {object}
+   */
+  getFields() {
+    const fields = {};
+    this.config.fields.forEach(fieldRef => {
+      fields[fieldRef] = this.bid.entities.fields(fieldRef);
+    });
+    return fields;
+  }
 
-    /**
-     * Adds the field group to an assembly.
-     *
-     * @param {Assembly|string} assembly The assembly entity or an assembly ref id
-     * @return {Assembly} the new assembly setting
-     */
-    setAssembly(assembly) {
-        if (!assembly) throw new Error('Assembly reference was not provided.');
-        setAssembly(this, assembly);
-        this.dirty();
-        return this.getAssembly();
-    }
+  /*
+   * Get the field group's assembly if it has one
+   *
+   * @return {Assembly|undefined}
+   */
+  getAssembly() {
+    return getAssembly(this);
+  }
 
-    /**
-     * Removes any assembly reference from the field group.
-     *
-     * @return {void}
-     */
-    unsetAssembly() {
-        setAssembly(this, null);
-        this.dirty();
-    }
+  /**
+   * Adds the field group to an assembly.
+   *
+   * @param {Assembly|string} assembly The assembly entity or an assembly ref id
+   * @return {Assembly} the new assembly setting
+   */
+  setAssembly(assembly) {
+    if (!assembly) throw new Error("Assembly reference was not provided.");
+    setAssembly(this, assembly);
+    this.dirty();
+    return this.getAssembly();
+  }
 
-    /**
-     * Determines if the field group is changed for it's original data.
-     * 
-     * @returns {boolean} 
-     */
-    isDirty() {
-        return this._is_dirty || !isEqual(this._data.config, this._original.config);
-    }
+  /**
+   * Removes any assembly reference from the field group.
+   *
+   * @return {void}
+   */
+  unsetAssembly() {
+    setAssembly(this, null);
+    this.dirty();
+  }
 
-    /**
-     * Flags the field group and corresponding bid as dirty and to be saved.
-     */
-    dirty() {
-        this.bid.dirty();
-        super.dirty();
-    }
+  /**
+   * Determines if the field group is changed for it's original data.
+   *
+   * @returns {boolean}
+   */
+  isDirty() {
+    return this._is_dirty || !isEqual(this._data.config, this._original.config);
+  }
+
+  /**
+   * Flags the field group and corresponding bid as dirty and to be saved.
+   */
+  dirty() {
+    this.bid.dirty();
+    super.dirty();
+  }
 }
