db32d48005c177d642c693730eb8c3d948a68405
Ryan Elliott <ryan@pvbid.com>
2019-09-10 14:00:38 +0000

    Merged in issue/PV-1749-undefined-rule-dependency (pull request #78)
    issue/PV-1749-undefined-rule-dependency
    
    * only consider rule dependency if its used
    
    Approved-by: Randen Kelly

--- src/domain/services/LineItemRuleService.js
+++ src/domain/services/LineItemRuleService.js
@@ -2,127 +2,128 @@ import _ from "lodash";
 import Helpers from "../../utils/Helpers";
 
 export default class LineItemRuleService {
-    isIncluded(lineItem) {
-        var includeStatues = [];
+  isIncluded(lineItem) {
+    var includeStatues = [];
 
-        if (lineItem.config.rules.length > 0) {
-            for (let rule of Object.values(lineItem.config.rules)) {
-                if (
-                    lineItem.config.rule_inclusion === "all" ||
-                    (lineItem.config.rule_inclusion === "any" && !_.includes(includeStatues, true))
-                ) {
-                    if (this._isUsingUndefinedDependencies(rule, lineItem)) {
-                        // For predictive pricing, undefined rule references default to true because
-                        //  it is better for the predicted cost to come out high than low.
-                        if (lineItem.bid.entities.variables().predictive_pricing.value) {
-                            includeStatues.push(true);
-                        } else {
-                            includeStatues.push(false);
-                        }
-                    } else if (rule.type === "always_include") {
-                        includeStatues.push(true);
-                    } else if (rule.type === "value_expression") {
-                        includeStatues.push(this._evalExpressionRule(rule, lineItem));
-                    } else if (rule.type === "toggle_field") {
-                        includeStatues.push(this._evalToggleFieldRule(rule, lineItem));
-                    } else if (rule.type === "list_field") {
-                        includeStatues.push(this._evalListSelectRule(rule, lineItem));
-                    }
-                }
+    if (lineItem.config.rules.length > 0) {
+      for (let rule of Object.values(lineItem.config.rules)) {
+        if (
+          lineItem.config.rule_inclusion === "all" ||
+          (lineItem.config.rule_inclusion === "any" && !_.includes(includeStatues, true))
+        ) {
+          if (this._isUsingUndefinedDependencies(rule, lineItem)) {
+            // For predictive pricing, undefined rule references default to true because
+            //  it is better for the predicted cost to come out high than low.
+            if (lineItem.bid.entities.variables().predictive_pricing.value) {
+              includeStatues.push(true);
+            } else {
+              includeStatues.push(false);
             }
-
-            return lineItem.config.rule_inclusion === "all"
-                ? !_.includes(includeStatues, false)
-                : _.includes(includeStatues, true);
-        } else return false;
-    }
-
-    /**
-     * Determine if the line item should be weighted by examining the rules
-     * A line item will be weighted if predictive pricing is enabled AND a rule dependency is not defined
-     *
-     * @param {BidEntity} lineItem
-     * @return {boolean}
-     */
-    isWeighted(lineItem) {
-        if (lineItem.bid.entities.variables().predictive_pricing.value) {
-            if (lineItem.config.rules.length > 0) {
-                for (let rule of Object.values(lineItem.config.rules)) {
-                    if (this._isUsingUndefinedDependencies(rule, lineItem)) {
-                        return true;
-                    }
-                }
-            }
-            return false;
+          } else if (rule.type === "always_include") {
+            includeStatues.push(true);
+          } else if (rule.type === "value_expression") {
+            includeStatues.push(this._evalExpressionRule(rule, lineItem));
+          } else if (rule.type === "toggle_field") {
+            includeStatues.push(this._evalToggleFieldRule(rule, lineItem));
+          } else if (rule.type === "list_field") {
+            includeStatues.push(this._evalListSelectRule(rule, lineItem));
+          }
         }
-        return false;
-    }
+      }
 
-    /**
-     * Determine if a rule is relying on any undefined dependency values
-     *
-     * @param {object} rule The line item rule
-     * @param {object} lineItem The line item
-     * @return {boolean}
-     */
-    _isUsingUndefinedDependencies(rule, lineItem) {
-        // handle special cases
-        if (rule.type === "always_include") return false;
-        if (rule.type === "list_field") {
-            const dep = lineItem.bid.entities.getDependency(rule.dependencies.list_field);
-            return !dep.value;
+      return lineItem.config.rule_inclusion === "all"
+        ? !_.includes(includeStatues, false)
+        : _.includes(includeStatues, true);
+    } else return false;
+  }
+
+  /**
+   * Determine if the line item should be weighted by examining the rules
+   * A line item will be weighted if predictive pricing is enabled AND a rule dependency is not defined
+   *
+   * @param {BidEntity} lineItem
+   * @return {boolean}
+   */
+  isWeighted(lineItem) {
+    if (lineItem.bid.entities.variables().predictive_pricing.value) {
+      if (lineItem.config.rules.length > 0) {
+        for (let rule of Object.values(lineItem.config.rules)) {
+          if (this._isUsingUndefinedDependencies(rule, lineItem)) {
+            return true;
+          }
         }
+      }
+      return false;
+    }
+    return false;
+  }
 
-        // get dependencies
-        let dependencies = [];
-        if (rule.type === "value_expression") {
-            if (rule.dependencies.a.type) dependencies.push(rule.dependencies.a);
-            if (rule.dependencies.b.type) dependencies.push(rule.dependencies.b);
-        } else if (rule.type === "toggle_field") {
-            dependencies.push(rule.dependencies.toggle_field);
-        }
-
-        // check dependencies
-        for (let i = 0; i < dependencies.length; i += 1) {
-            const isFullyDefined = lineItem.bid.entities.isDependencyFullyDefined(dependencies[i]);
-            const isValidValue = !_.isNil(lineItem.bid.entities.getDependencyValue(dependencies[i]))
-            if (!isFullyDefined || !isValidValue) {
-                return true;
-            }
-        }
-        return false;
+  /**
+   * Determine if a rule is relying on any undefined dependency values
+   *
+   * @param {object} rule The line item rule
+   * @param {object} lineItem The line item
+   * @return {boolean}
+   */
+  _isUsingUndefinedDependencies(rule, lineItem) {
+    // handle special cases
+    if (rule.type === "always_include") return false;
+    if (rule.type === "list_field") {
+      const dep = lineItem.bid.entities.getDependency(rule.dependencies.list_field);
+      return !dep.value;
     }
 
-    _evalExpressionRule(lineItemRule, lineItem) {
-        const a = lineItem.bid.entities.getDependencyValue(lineItemRule.dependencies.a);
-        const b = lineItem.bid.entities.getDependencyValue(lineItemRule.dependencies.b);
-
-        const valueMap = {
-            a: Helpers.confirmNumber(a, 0),
-            b: Helpers.confirmNumber(b, 0),
-        };
-
-        const results = Helpers.evalExpression(lineItemRule.expression, valueMap);
-
-        if (!_.isNaN(results) && _.isBoolean(results)) {
-            return lineItemRule.activate_on ? results : !results;
-        } else return false;
+    // get dependencies
+    let dependencies = [];
+    if (rule.type === "value_expression") {
+      const args = Helpers.parseFormulaArguments(rule.expression);
+      if (rule.dependencies.a.type && args.includes("a")) dependencies.push(rule.dependencies.a);
+      if (rule.dependencies.b.type && args.includes("b")) dependencies.push(rule.dependencies.b);
+    } else if (rule.type === "toggle_field") {
+      dependencies.push(rule.dependencies.toggle_field);
     }
 
-    _evalToggleFieldRule(lineItemRule, lineItem) {
-        let toggleValue = lineItem.bid.entities.getDependencyValue(lineItemRule.dependencies.toggle_field);
-
-        toggleValue = toggleValue === "0" || toggleValue === "1" ? Boolean(parseInt(toggleValue)) : toggleValue;
-
-        if (!_.isNaN(toggleValue) && _.isBoolean(toggleValue)) {
-            return lineItemRule.activate_on ? toggleValue : !toggleValue;
-        } else return false;
+    // check dependencies
+    for (let i = 0; i < dependencies.length; i += 1) {
+      const isFullyDefined = lineItem.bid.entities.isDependencyFullyDefined(dependencies[i]);
+      const isValidValue = !_.isNil(lineItem.bid.entities.getDependencyValue(dependencies[i]));
+      if (!isFullyDefined || !isValidValue) {
+        return true;
+      }
     }
+    return false;
+  }
 
-    _evalListSelectRule(lineItemRule, lineItem) {
-        const listField = lineItem.bid.entities.getDependency(lineItemRule.dependencies.list_field);
-        const hasSelected = _.includes(lineItemRule.list_options, listField.value);
+  _evalExpressionRule(lineItemRule, lineItem) {
+    const a = lineItem.bid.entities.getDependencyValue(lineItemRule.dependencies.a);
+    const b = lineItem.bid.entities.getDependencyValue(lineItemRule.dependencies.b);
 
-        return lineItemRule.activate_on ? hasSelected : !hasSelected;
-    }
+    const valueMap = {
+      a: Helpers.confirmNumber(a, 0),
+      b: Helpers.confirmNumber(b, 0),
+    };
+
+    const results = Helpers.evalExpression(lineItemRule.expression, valueMap);
+
+    if (!_.isNaN(results) && _.isBoolean(results)) {
+      return lineItemRule.activate_on ? results : !results;
+    } else return false;
+  }
+
+  _evalToggleFieldRule(lineItemRule, lineItem) {
+    let toggleValue = lineItem.bid.entities.getDependencyValue(lineItemRule.dependencies.toggle_field);
+
+    toggleValue = toggleValue === "0" || toggleValue === "1" ? Boolean(parseInt(toggleValue)) : toggleValue;
+
+    if (!_.isNaN(toggleValue) && _.isBoolean(toggleValue)) {
+      return lineItemRule.activate_on ? toggleValue : !toggleValue;
+    } else return false;
+  }
+
+  _evalListSelectRule(lineItemRule, lineItem) {
+    const listField = lineItem.bid.entities.getDependency(lineItemRule.dependencies.list_field);
+    const hasSelected = _.includes(lineItemRule.list_options, listField.value);
+
+    return lineItemRule.activate_on ? hasSelected : !hasSelected;
+  }
 }
--- tests/line_item_rules.test.js
+++ tests/line_item_rules.test.js
@@ -1,240 +1,216 @@
-//const PVBid = require("../src/pvbid.js");
-//import { pvbid } from "../src/pvbid";
-import _ from "lodash";
-import jest from "jest";
-var axios = require("axios");
-var jsonfile = require("jsonfile");
-const PVBid = require("../src/pvbid.js");
-var MockAdapter = require("axios-mock-adapter");
-import LineItemScaffolding from "../src/domain/scaffolding/LineItemScaffolding";
-
-let context = PVBid.createContext({ token: "Bearer Token", base_uri: "http://api.pvbid.local/v2" });
-let project, bid, field, lineItem, metric;
+import { loadTestProject } from "./TestProjectLoader";
+let project, bid, field, lineItem;
 
 describe("Testing Line Item Rules", () => {
-    beforeAll(() => {
-        return init();
+  beforeAll(async () => {
+    project = await loadTestProject();
+    bid = project.bids[190];
+    await bid.reassessAsync();
+  });
+
+  test("line item should be excluded when no rules are available", async () => {
+    expect.assertions(1);
+
+    return new Promise(resolve => {
+      let lineItem = bid.entities.searchByTitle("line_item", "General Line Item")[0];
+      lineItem.once("assessed", () => {
+        expect(lineItem.isIncluded).toBe(false);
+        resolve();
+      });
+      lineItem.config.rules = [];
+      lineItem.assess();
+    });
+  });
+
+  describe("Rule type 'always_include'", () => {
+    test("should always include when enabled.", () => {
+      expect.assertions(3);
+      // line item should have no rules due to previous test.
+      let lineItem = bid.entities.searchByTitle("line_item", "General Line Item")[0];
+      expect(lineItem.config.rules).toEqual([]);
+      return new Promise(resolve => {
+        lineItem.once("assessed", () => {
+          expect(lineItem.config.rules[0].type).toBe("always_include");
+          expect(lineItem.isIncluded).toBe(true);
+          resolve();
+        });
+        lineItem.config.rules.push({ title: "Always Include", type: "always_include" });
+        lineItem.assess();
+      });
+    });
+  });
+  describe("Rule type expression", () => {
+    test("should include line item when (a > b)", async () => {
+      expect.assertions(5);
+
+      let lineItem = bid.entities.searchByTitle(
+        "line_item",
+        "On With Rule Value Expression - Watts > Wage"
+      )[0];
+      let wattsMetric = bid.entities.searchByTitle("metric", "watts")[0];
+      await new Promise(resolve => {
+        lineItem.once("assessed", () => {
+          expect(lineItem.isIncluded).toBe(false);
+          resolve();
+        });
+        wattsMetric.value = 0;
+      });
+
+      await new Promise(resolve => {
+        expect(lineItem.isIncluded).toBe(false);
+
+        lineItem.once("assessed", () => {
+          expect(lineItem.isIncluded).toBe(true);
+          resolve();
+        });
+        wattsMetric.value = 36;
+      });
+
+      await new Promise(resolve => {
+        expect(lineItem.isIncluded).toBe(true);
+
+        lineItem.once("assessed", () => {
+          expect(lineItem.isIncluded).toBe(false);
+          resolve();
+        });
+        wattsMetric.value = 15;
+      });
     });
 
-    async function init() {
-        // This sets the mock adapter on the default instance
-        var mock = new MockAdapter(axios);
+    test("should exclude line item when (a > b)", async () => {
+      expect.assertions(5);
 
-        // Mock any GET request to /users
-        // arguments for reply are (status, data, headers)
-        let mockedLineItem = LineItemScaffolding.create(190, "The New Line Item");
-        mockedLineItem.id = 1000001;
-        mock.onPost("http://api.pvbid.local/v2/bids/190/line_items/").reply(200, {
-            data: {
-                line_item: mockedLineItem
-            }
-        });
-        project = await new Promise(resolve => {
-            jsonfile.readFile("./tests/simple-test-project.json", (err, data) => {
-                mock.onGet("http://api.pvbid.local/v2/projects/461").reply(200, {
-                    data: { project: data.project }
-                });
-                mock.onGet("http://api.pvbid.local/v2/bids/190").reply(200, {
-                    data: { bid: data.bid }
-                });
-                mock.onGet("http://api.pvbid.local/v2/users/me").reply(200, {
-                    data: { user: data.user }
-                });
-                mock.onGet("http://api.pvbid.local/v2/predictions/").reply(200, {
-                data: { prediction_models: data.prediction_models }
-            });
+      let lineItem = bid.entities.searchByTitle(
+        "line_item",
+        "On With Rule Value Expression - Watts > Wage"
+      )[0];
+      lineItem.config.rules[0].activate_on = false;
 
-                context.getProject(461).then(p => {
-                    resolve(p);
-                });
-            });
+      let wattsMetric = bid.entities.searchByTitle("metric", "watts")[0];
+      await new Promise(resolve => {
+        lineItem.once("assessed", () => {
+          expect(lineItem.isIncluded).toBe(true);
+          resolve();
         });
-        return new Promise(resolve => {
-            bid = _.toArray(project.bids)[0];
-            project.on("assessed", "test", () => {
-                resolve();
-            });
-            bid.reassessAll(true);
-        });
-    }
+        wattsMetric.value = 0;
+      });
 
-    test("line item should be excluded when no rules are available", async () => {
-        expect.assertions(1);
+      await new Promise(resolve => {
+        expect(lineItem.isIncluded).toBe(true);
 
-        return new Promise(resolve => {
-            let lineItem = bid.entities.searchByTitle("line_item", "General Line Item")[0];
-            lineItem.once("assessed", () => {
-                expect(lineItem.isIncluded).toBe(false);
-                resolve();
-            });
-            lineItem.config.rules = [];
-            lineItem.assess();
+        lineItem.once("assessed", () => {
+          expect(lineItem.isIncluded).toBe(false);
+          resolve();
         });
+        wattsMetric.value = 36;
+      });
+
+      await new Promise(resolve => {
+        expect(lineItem.isIncluded).toBe(false);
+
+        lineItem.once("assessed", () => {
+          expect(lineItem.isIncluded).toBe(true);
+          resolve();
+        });
+        wattsMetric.value = 15;
+      });
+    });
+  });
+
+  describe("Rule toggle list", () => {
+    test("should exclude line item with no field selection", () => {
+      field = bid.entities.searchByTitle("field", "module type")[0];
+
+      expect.assertions(2);
+
+      lineItem = bid.entities.searchByTitle("line_item", "on with module selected")[0];
+      expect(lineItem.isIncluded).toBe(true);
+
+      return new Promise(resolve => {
+        lineItem.once("assessed", () => {
+          expect(lineItem.isIncluded).toBe(false);
+          resolve();
+        });
+        field.value = null;
+      });
     });
 
-    describe("Rule type 'always_include'", () => {
-        test("should always include when enabled.", () => {
-            expect.assertions(3);
-            // line item should have no rules due to previous test.
-            let lineItem = bid.entities.searchByTitle("line_item", "General Line Item")[0];
-            expect(lineItem.config.rules).toEqual([]);
-            return new Promise(resolve => {
-                lineItem.once("assessed", () => {
-                    expect(lineItem.config.rules[0].type).toBe("always_include");
-                    expect(lineItem.isIncluded).toBe(true);
-                    resolve();
-                });
-                lineItem.config.rules.push({ title: "Always Include", type: "always_include" });
-                lineItem.assess();
-            });
+    test("should include line item with field selection", () => {
+      expect.assertions(2);
+
+      return new Promise(resolve => {
+        expect(lineItem.isIncluded).toBe(false);
+
+        lineItem.once("assessed", () => {
+          expect(lineItem.isIncluded).toBe(true);
+          resolve();
         });
+        let options = field.getListOptions();
+        field.value = options[0].row_id;
+      });
     });
-    describe("Rule type expression", () => {
-        test("should include line item when (a > b)", async () => {
-            expect.assertions(5);
+  });
 
-            let lineItem = bid.entities.searchByTitle("line_item", "On With Rule Value Expression - Watts > Wage")[0];
-            let wattsMetric = bid.entities.searchByTitle("metric", "watts")[0];
-            await new Promise(resolve => {
-                lineItem.once("assessed", () => {
-                    expect(lineItem.isIncluded).toBe(false);
-                    resolve();
-                });
-                wattsMetric.value = 0;
-            });
-
-            await new Promise(resolve => {
-                expect(lineItem.isIncluded).toBe(false);
-
-                lineItem.once("assessed", () => {
-                    expect(lineItem.isIncluded).toBe(true);
-                    resolve();
-                });
-                wattsMetric.value = 36;
-            });
-
-            await new Promise(resolve => {
-                expect(lineItem.isIncluded).toBe(true);
-
-                lineItem.once("assessed", () => {
-                    expect(lineItem.isIncluded).toBe(false);
-                    resolve();
-                });
-                wattsMetric.value = 15;
-            });
-        });
-
-        test("should exclude line item when (a > b)", async () => {
-            expect.assertions(5);
-
-            let lineItem = bid.entities.searchByTitle("line_item", "On With Rule Value Expression - Watts > Wage")[0];
-            lineItem.config.rules[0].activate_on = false;
-
-            let wattsMetric = bid.entities.searchByTitle("metric", "watts")[0];
-            await new Promise(resolve => {
-                lineItem.once("assessed", () => {
-                    expect(lineItem.isIncluded).toBe(true);
-                    resolve();
-                });
-                wattsMetric.value = 0;
-            });
-
-            await new Promise(resolve => {
-                expect(lineItem.isIncluded).toBe(true);
-
-                lineItem.once("assessed", () => {
-                    expect(lineItem.isIncluded).toBe(false);
-                    resolve();
-                });
-                wattsMetric.value = 36;
-            });
-
-            await new Promise(resolve => {
-                expect(lineItem.isIncluded).toBe(false);
-
-                lineItem.once("assessed", () => {
-                    expect(lineItem.isIncluded).toBe(true);
-                    resolve();
-                });
-                wattsMetric.value = 15;
-            });
-        });
+  describe("Determine if line item should be weighted", () => {
+    afterAll(() => {
+      lineItem.bid.entities.variables().predictive_pricing.value = false;
+      lineItem.bid.entities.variables().wage.value = 35;
     });
 
-    describe("Rule toggle list", () => {
-        test("should exclude line item with no field selection", () => {
-            field = bid.entities.searchByTitle("field", "module type")[0];
+    test("Line item should not be weighted if predictive pricing is off", () => {
+      expect.assertions(1);
+      lineItem.bid.entities.variables().predictive_pricing.value = false;
+      lineItem.reset();
 
-            expect.assertions(2);
-
-            lineItem = bid.entities.searchByTitle("line_item", "on with module selected")[0];
-            expect(lineItem.isIncluded).toBe(true);
-
-            return new Promise(resolve => {
-                lineItem.once("assessed", () => {
-                    expect(lineItem.isIncluded).toBe(false);
-                    resolve();
-                });
-                field.value = null;
-            });
-        });
-
-        test("should include line item with field selection", () => {
-            expect.assertions(2);
-
-            return new Promise(resolve => {
-                expect(lineItem.isIncluded).toBe(false);
-
-                lineItem.once("assessed", () => {
-                    expect(lineItem.isIncluded).toBe(true);
-                    resolve();
-                });
-                let options = field.getListOptions();
-                field.value = options[0].row_id;
-            });
-        });
+      expect(lineItem.isWeighted).toBe(false);
     });
 
-    describe("Determine if line item should be weighted", () => {
-        afterAll(() => {
-            lineItem.bid.entities.variables().predictive_pricing.value = false;
-            lineItem.bid.entities.variables().wage.value = 35;
-        });
+    test("Line item should not be weighted if predictive pricing is on but all rules are defined", () => {
+      expect.assertions(1);
+      lineItem.bid.entities.variables().predictive_pricing.value = true;
+      lineItem.reset();
 
-        test("Line item should not be weighted if predictive pricing is off", () => {
-            expect.assertions(1);
-            lineItem.bid.entities.variables().predictive_pricing.value = false;
-            lineItem.reset();
-
-            expect(lineItem.isWeighted).toBe(false);
-        });
-
-        test("Line item should not be weighted if predictive pricing is on but all rules are defined", () => {
-            expect.assertions(1);
-            lineItem.bid.entities.variables().predictive_pricing.value = true;
-            lineItem.reset();
-
-            expect(lineItem.isWeighted).toBe(false);
-        });
-
-        test("Line item should not be weighted if it is not predicted and a rule is undefined", () => {
-            let $lineItem = bid.entities.searchByTitle("line_item", "On With Rule Value Expression - Watts > Wage")[0];
-            expect.assertions(1);
-            $lineItem.bid.entities.variables().predictive_pricing.value = true;
-            $lineItem.bid.entities.variables().wage.value = null;
-            $lineItem.reset();
-
-            expect($lineItem.isWeighted).toBe(false);
-        });
-
-        test("Line item should be weighted if it is predicted and a rule is undefined", () => {
-            let $lineItem = bid.entities.searchByTitle("line_item", "On With Rule Value Expression - Watts > Wage")[0];
-            expect.assertions(1);
-            $lineItem.bid.entities.variables().predictive_pricing.value = true;
-            $lineItem.bid.entities.variables().wage.value = null;
-            $lineItem.useComputedValueWhenAvailable = false;
-
-            expect($lineItem.isWeighted).toBe(true);
-        });
+      expect(lineItem.isWeighted).toBe(false);
     });
+
+    test("Line item should not be weighted if it is not predicted and a rule is undefined", () => {
+      let $lineItem = bid.entities.searchByTitle(
+        "line_item",
+        "On With Rule Value Expression - Watts > Wage"
+      )[0];
+      expect.assertions(1);
+      $lineItem.bid.entities.variables().predictive_pricing.value = true;
+      $lineItem.bid.entities.variables().wage.value = null;
+      $lineItem.reset();
+
+      expect($lineItem.isWeighted).toBe(false);
+    });
+
+    test("Line item should be weighted if it is predicted and a rule is undefined", () => {
+      let $lineItem = bid.entities.searchByTitle(
+        "line_item",
+        "On With Rule Value Expression - Watts > Wage"
+      )[0];
+      expect.assertions(1);
+      $lineItem.bid.entities.variables().predictive_pricing.value = true;
+      $lineItem.bid.entities.variables().wage.value = null;
+      $lineItem.useComputedValueWhenAvailable = false;
+
+      expect($lineItem.isWeighted).toBe(true);
+    });
+
+    test("Line item should not be weighted if it is predicted but a rule does not depend on the undefined dependency", () => {
+      let $lineItem = bid.entities.searchByTitle(
+        "line_item",
+        "On With Rule Value Expression - Watts > Wage"
+      )[0];
+      expect.assertions(1);
+      $lineItem.config.rules[0].expression = 1;
+      $lineItem.bid.entities.variables().predictive_pricing.value = true;
+      $lineItem.bid.entities.variables().wage.value = null;
+      $lineItem.useComputedValueWhenAvailable = false;
+
+      expect($lineItem.isWeighted).toBe(false);
+    });
+  });
 });
