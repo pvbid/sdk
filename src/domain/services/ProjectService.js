bf3c0b8afe669769de14dab0025d0734d2ac8638
Ryan Elliott <ryan@pvbid.com>
2020-04-09 15:34:55 +0000

    Merged in feature/PV-2508-autosave-query-flag (pull request #103)
    feature/PV-2508-autosave-query-flag
    
    * batchUpdate auto save flag
    
    * allow auto save to be disabled
    
    * update to 1.2.16-0
    
    Approved-by: Randen Kelly

--- src/domain/services/ProjectService.js
+++ src/domain/services/ProjectService.js
@@ -1,4 +1,6 @@
-import _ from "lodash";
+import filter from "lodash/filter";
+import keyBy from "lodash/keyBy";
+import each from "lodash/each";
 import BidFactory from "../factories/BidFactory";
 import ProjectSavingHelper from "./ProjectSavingHelper";
 
@@ -8,91 +10,93 @@ import ProjectSavingHelper from "./ProjectSavingHelper";
  * @param {BidService} bidService
  */
 export default class ProjectService {
-    /**
-     * Creates an instance of ProjectService.
-     * @param {PVBidContext} context
-     */
-    constructor(context) {
-        this.context = context;
-        this.repositories = context.repositories;
-        this._savingHelper = new ProjectSavingHelper();
+  /**
+   * Creates an instance of ProjectService.
+   * @param {PVBidContext} context
+   */
+  constructor(context) {
+    this.context = context;
+    this.repositories = context.repositories;
+    this._savingHelper = new ProjectSavingHelper();
+  }
+
+  /**
+   * Saves project and underlying bids.
+   *
+   * @param {Project} project
+   * @param {object} options
+   * @param {boolean} options.isAutoSave indicate if this is the result of an auto-save
+   * @returns {Promise<null>}
+   */
+  async save(project, { isAutoSave = false } = {}) {
+    const properties = [
+      "line_items",
+      "fields",
+      "components",
+      "metrics",
+      "field_groups",
+      "assemblies",
+      "component_groups",
+      "dynamic_groups",
+      "datatables",
+    ];
+    const promises = [];
+
+    const exported = this._savingHelper.extract(project);
+    const exportedJSON = JSON.stringify(exported);
+    const bidIds = Object.keys(exported.bids);
+    if (bidIds.length > 0) {
+      for (let i = 0; i < bidIds.length; i++) {
+        const bid = exported.bids[bidIds[i]];
+
+        const toSave = {
+          bids: {},
+          project: exported.project,
+        };
+        toSave.bids[bid.id] = bid;
+
+        properties.forEach(key => {
+          const filtered = filter(exported[key], el => el.bid_id === bid.id);
+          toSave[key] = keyBy(filtered, "id");
+        });
+
+        promises.push(this.repositories.projects.batchUpdate(project.id, toSave, { isAutoSave }));
+      }
+    } else if (exported.project.id) {
+      // project id will not be defined if not project data needs to be saved.
+      promises.push(this.repositories.projects.save(exported.project));
     }
+    await Promise.all(promises);
 
-    /**
-     * Saves project and underlying bids.
-     *
-     * @param {Project} project
-     * @returns {Promise<null>}
-     */
-    async save(project) {
-        const properties = [
-            "line_items",
-            "fields",
-            "components",
-            "metrics",
-            "field_groups",
-            "assemblies",
-            "component_groups",
-            "dynamic_groups",
-            "datatables"
-        ];
-        const promises = [];
-
-        const exported = this._savingHelper.extract(project);
-        const exportedJSON = JSON.stringify(exported);
-        const bidIds = Object.keys(exported.bids);
-        if (bidIds.length > 0) {
-            for (let i = 0; i < bidIds.length; i++) {
-                const bid = exported.bids[bidIds[i]];
-
-                const toSave = {
-                    bids: {},
-                    project: exported.project
-                };
-                toSave.bids[bid.id] = bid;
-
-                properties.forEach(key => {
-                    const filtered = _.filter(exported[key], el => el.bid_id === bid.id);
-                    toSave[key] = _.keyBy(filtered, "id");
-                });
-
-                promises.push(this.repositories.projects.batchUpdate(project.id, toSave));
-            }
-        } else if (exported.project.id) {
-            // project id will not be defined if not project data needs to be saved.
-            promises.push(this.repositories.projects.save(exported.project));
-        }
-        await Promise.all(promises);
-
-        // check for new changes before clearing dirty flags because changes may have been made
-        // while saving the project asynchonously
-        const compareJSON = JSON.stringify(this._savingHelper.extract(project));
-        if (compareJSON === exportedJSON) {
-            _.each(project.bids, bid => {
-                bid.pristine();
-            });
-            project.pristine();
-        }
+    // check for new changes before clearing dirty flags because changes may have been made
+    // while saving the project asynchonously
+    const compareJSON = JSON.stringify(this._savingHelper.extract(project));
+    if (compareJSON === exportedJSON) {
+      each(project.bids, bid => {
+        bid.pristine();
+      });
+      project.pristine();
     }
+  }
 
-    /**
-     * Creates bid and attaches to project.
-     *
-     * @param {Project} project
-     * @param {string} [title=New Bid]
-     * @returns {Promise<Bid>}
-     */
-    async createBid(project, title) {
-        try {
-            title = title ? title : "New Bid";
-            const bidData = await this.repositories.bids.create({ title: title, project_id: project.id });
-            const bid = new BidFactory().create(bidData, this.context, project);
-            bid.isActive = true;
-            project.attachBid(bid);
-            project.assess();
-            return bid;
-        } catch (error) {
-            return Promise.reject(error);
-        }
+  /**
+   * Creates bid and attaches to project.
+   *
+   * @param {Project} project
+   * @param {string} [title=New Bid]
+   * @returns {Promise<Bid>}
+   */
+  async createBid(project, title) {
+    try {
+      title = title ? title : "New Bid";
+      const bidData = await this.repositories.bids.create({ title: title, project_id: project.id });
+      const bid = new BidFactory().create(bidData, this.context, project);
+      bid.isActive = true;
+      project.attachBid(bid);
+      project.assess();
+      return bid;
+    } catch (error) {
+      return Promise.reject(error);
     }
+  }
 }
--- src/repositories/ProjectRepository.js
+++ src/repositories/ProjectRepository.js
@@ -1,101 +1,104 @@
 import BaseRepository from "./BaseRepository";
 
 export default class ProjectRepository extends BaseRepository {
-    constructor(config) {
-        super(config.base_uri + "/projects/", "project", "projects", config);
-    }
+  constructor(config) {
+    super(config.base_uri + "/projects/", "project", "projects", config);
+  }
 
-    /**
-     * Retrieves a list of projects
-     *
-     * @param {Object} [params] A set of parameters to filter the projects by.
-     * @param {number} params.per_page Number of projects to to display per request (max 100)
-     * @param {number} params.page Page number
-     * @param {string} params.search Search term to apply to project's title
-     * @param {string} params.sort_order 'asc' or 'desc'
-     * @param {string} params.order_by The name of the property to sort the results by
-     * @param {number[]} params.tag_ids List of tags to filter by (excluding this will include all tags)
-     * @param {number[]} params.user_ids List of users to filter by (excluding this will include all users)
-     * @return {Promise<BidEntity[]>} Filtered list of projects
-     */
-    async get(params) {
-        return super.get(params);
-    }
+  /**
+   * Retrieves a list of projects
+   *
+   * @param {Object} [params] A set of parameters to filter the projects by.
+   * @param {number} params.per_page Number of projects to to display per request (max 100)
+   * @param {number} params.page Page number
+   * @param {string} params.search Search term to apply to project's title
+   * @param {string} params.sort_order 'asc' or 'desc'
+   * @param {string} params.order_by The name of the property to sort the results by
+   * @param {number[]} params.tag_ids List of tags to filter by (excluding this will include all tags)
+   * @param {number[]} params.user_ids List of users to filter by (excluding this will include all users)
+   * @return {Promise<BidEntity[]>} Filtered list of projects
+   */
+  async get(params) {
+    return super.get(params);
+  }
 
-    /**
-     * Adds a bid to a project
-     *
-     * @param {number} projectId The project's id
-     * @param {number} bidId The bid's id
-     * @return {Promise<Object>} API response status
-     */
-    async attachBid(projectId, bidId) {
-        return this.http.post(this.endpoint + projectId + "/bids/" + bidId).then(function(response) {
-            return response.data.data;
-        });
-    }
+  /**
+   * Adds a bid to a project
+   *
+   * @param {number} projectId The project's id
+   * @param {number} bidId The bid's id
+   * @return {Promise<Object>} API response status
+   */
+  async attachBid(projectId, bidId) {
+    return this.http.post(this.endpoint + projectId + "/bids/" + bidId).then(function(response) {
+      return response.data.data;
+    });
+  }
 
-    /**
-     * Removes a bid from a project
-     *
-     * @param {number} projectId The project's id
-     * @param {number} bidId The bid's id
-     * @return {Promise<Object>} API response status
-     */
-    async detachBid(projectId, bidId) {
-        return this.http.delete(this.endpoint + projectId + "/bids/" + bidId).then(function(response) {
-            return response.data.data;
-        });
-    }
+  /**
+   * Removes a bid from a project
+   *
+   * @param {number} projectId The project's id
+   * @param {number} bidId The bid's id
+   * @return {Promise<Object>} API response status
+   */
+  async detachBid(projectId, bidId) {
+    return this.http.delete(this.endpoint + projectId + "/bids/" + bidId).then(function(response) {
+      return response.data.data;
+    });
+  }
 
-    /**
-     * Adds a user to a project
-     *
-     * @param {number} projectId The project's id
-     * @param {number} userId The user's id
-     * @return {Promise<Object>} API response status
-     */
-    async attachUser(projectId, userId) {
-        return this.http.post(this.endpoint + projectId + "/users/" + userId).then(function(response) {
-            return response.data.data;
-        });
-    }
+  /**
+   * Adds a user to a project
+   *
+   * @param {number} projectId The project's id
+   * @param {number} userId The user's id
+   * @return {Promise<Object>} API response status
+   */
+  async attachUser(projectId, userId) {
+    return this.http.post(this.endpoint + projectId + "/users/" + userId).then(function(response) {
+      return response.data.data;
+    });
+  }
 
-    /**
-     * Removes a user from a project
-     *
-     * @param {number} projectId The project's id
-     * @param {number} userId The user's id
-     * @return {Promise<Object>} API response status
-     */
-    async detachUser(projectId, userId) {
-        return this.http.delete(this.endpoint + projectId + "/users/" + userId).then(function(response) {
-            return response.data.data;
-        });
-    }
+  /**
+   * Removes a user from a project
+   *
+   * @param {number} projectId The project's id
+   * @param {number} userId The user's id
+   * @return {Promise<Object>} API response status
+   */
+  async detachUser(projectId, userId) {
+    return this.http.delete(this.endpoint + projectId + "/users/" + userId).then(function(response) {
+      return response.data.data;
+    });
+  }
 
-    /**
-     * Allows persistance of underlying bid data along with project
-     *
-     * @param {number} projectId 
-     * @param {Object} data The project data to update
-     * @return {Promise<Object>} The API response
-     */
-    async batchUpdate(projectId, data) {
-        return this.http.put(this.endpoint + projectId + "/batch/", data).then(function(response) {
-            return response.data.data;
-        });
-    }
+  /**
+   * Allows persistance of underlying bid data along with project
+   *
+   * @param {number} projectId
+   * @param {Object} data The project data to update
+   * @param {object} options
+   * @param {boolean} options.isAutoSave Indicates that this batch update is due to the auto-save. Useful for debugging.
+   * @return {Promise<Object>} The API response
+   */
+  async batchUpdate(projectId, data, { isAutoSave = false } = {}) {
+    const config = isAutoSave ? { params: { is_auto: isAutoSave } } : {};
+    return this.http
+      .put(this.endpoint + projectId + "/batch/", data, config)
+      .then(response => response.data.data);
+  }
 
-    /**
-     * Clones a project
-     *
-     * @param {number} projectId
-     * @return {Promise<BidEntity>} Project clone
-     */
-    async clone(projectId) {
-        return this.http.post(this.endpoint + projectId + "/clone").then(function(response) {
-            return response.data.data.project;
-        });
-    }
+  /**
+   * Clones a project
+   *
+   * @param {number} projectId
+   * @return {Promise<BidEntity>} Project clone
+   */
+  async clone(projectId) {
+    return this.http.post(this.endpoint + projectId + "/clone").then(function(response) {
+      return response.data.data.project;
+    });
+  }
 }
